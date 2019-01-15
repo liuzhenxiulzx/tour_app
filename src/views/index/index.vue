d03a3c189cc13982f0cba8b5cbfb1f8b487311e0
lzx <2603757946@qq.com>
2019-01-15 15:43:50 +0800

    点赞整理

--- src/views/index/index.vue
+++ src/views/index/index.vue
@@ -16,17 +16,11 @@
         </wv-swipe-item>
       </wv-swipe>
     </header>
-    <div class="nav">
-      <wv-tabs swipeable>
-        <wv-tab v-for="(v,k) in titlebar" :key="k" :title="v.title">内容</wv-tab>
-      </wv-tabs>
-    </div>
+
     <div class="content">
       <ul>
-        
-        <li  v-for="(v,k) in blog" :key="k">
-          <!-- <router-link :to="{path:'details',params:{id:v.id} }" class="cent_abstract" > -->
-          <router-link :to="'/details/'+v.id" class="cent_abstract" >
+        <li v-for="(v,k) in blog" :key="k">
+          <router-link :to="'/details/'+v.id" class="cent_abstract">
             <div class="cent_img">
               <img :src="v.article_img" alt>
             </div>
@@ -34,31 +28,30 @@
               <a href="#">{{v.content}}</a>
             </div>
           </router-link>
-            <div class="operation">
-              <div class="comment">
-                <img v-if="dianzhan" src="../../assets/images/index_like.png" @click="switchbgi(k)" alt>
-                <img  v-else src="../../assets/images/like.png" @click="switchbgi(k)" alt>
-                <span>{{v.goods_number}}</span>
-              </div>
-              <div class="thumbs-up" >
-                <img  src="../../assets/images/comment29.png" >
-                <span >{{v.comment_number}}</span>
-              </div>
-              <div class="share">
-                <img src="../../assets/images/fenxiang.png">
-                <span >23</span>
-              </div>
+          <div class="operation">
+            <div class="comment">
+              <img v-if="agree.isgoodup==0" src="../../assets/images/index_like.png" @click="switchbgi(v.id,k)" alt>
+              <img v-else src="../../assets/images/like.png" @click="switchbgi(v.id,k)" alt>
+              <span>{{v.goods_number}}</span>
             </div>
-            <div class="line"></div>
-            <div class="user">
-              <img src="../../assets/images/lunbo_04.jpg" alt>
-              <span>夏目南生</span>
-              <!-- <span class="time">{{v.created_at}}</span> -->
+            <div class="thumbs-up">
+              <img src="../../assets/images/comment29.png">
+              <span>{{v.comment_number}}</span>
             </div>
+            <div class="share">
+              <img src="../../assets/images/fenxiang.png">
+              <span>23</span>
+            </div>
+          </div>
+          <div class="line"></div>
+          <div class="user">
+            <img src="../../assets/images/lunbo_04.jpg" alt>
+            <span>夏目南生</span>
+          </div>
         </li>
-
       </ul>
     </div>
+
     <div class="interval"></div>
   </div>
 </template>
@@ -69,100 +62,66 @@
 </style>
 
 <script>
+import { Toast } from "we-vue";
+import { Dialog } from "we-vue";
 export default {
-  data(){
-    return{
-      titlebar:[
-        {
-          title:'最新',
-        },
-        {
-          title:'热门',
-        },
-        {
-          title:'排行',
-        }
-      ],
-      blog:[],
-      agree:
-        {
-          upuser_id:3,
-          article_id:1,
-          isgoodup:0,//是否点赞
-        },
-      dianzhan:false,
-      goodnum:0,//点击数量
-    }
+  data() {
+    return {
+      blog: [],
+      agree: {
+        upuser_id: localStorage.getItem("USER_ID"),
+        article_id: "",
+        isgoodup: "0" //是否点赞
+      },
+    };
   },
-  computed:{
-
-  },
-  created:function(){
-    this.axios.get('/showblog')
-    .then(res=>{
-      console.log(res.data.data);
+  computed: {},
+  created: function() {
+    this.axios.get("/showblog").then(res => {
       this.blog = res.data.data;
-    })
+    });
   },
-  methods:{
-      switchbgi(key){
-        
-
-        // this.goodup.upuser_id = localStorage.getItem('USER_ID')
-        // this.goodup.article_id = key +1
-        // this.dianzhan  = !this.dianzhan
-        // console.log(this.goodup);
-
-        // this.axios.post('/addagree/'.key+1,)
-        // .then(res=>{
-        //     console.log(res.data)
-        // })
-        // .catch(error=>{
-        //   console.log(error)
-        // })
-
-        // console.log(localStorage.getItem('USER_ID'))
-        // console.log(this.blog[key].goodups[0].isgoodup)
-        // if(this.blog[key].goodups.isgoodup){
-        //   alert(1)
-        // }else{
-        //   alert(2)
-        // }
-        // this.isdianzan = !this.isdianzan;
-        // 1.判断是否点赞
-          // 点过赞时
-        // if(this.goodup.isgoodup){
-        //     this.goodup.isgoodup = !this.goodup.isgoodup;
-        //     this.blog[key].goods_number--
-        //     alert('取消点赞'+this.blog[key].goods_number)
-        //     localStorage.setItem('goodstate',this.goodup.goodstate==false)
-        // }else{
-        //   // 没点赞时设置点赞状态为true
-        //   this.goodup.isgoodup = !this.goodup.isgoodup;
-        //   this.blog[key].goods_number++
-        //   alert('点赞'+this.blog[key].goods_number)
-        //   localStorage.setItem('goodstate',this.goodup.goodstate==true)
-        // }
-        // 2.如果没有点赞则把数量加一，如果二次点赞怎为取消点赞，并保存点赞状态
-        // 3.保存点赞的状态到浏览器
-
-        // if(this.goodup.isgoodup){
-        //   alert(1);
-        //   this.blog[key].goods_number = this.blog[key].goods_number + 1
-        // }else{
-        //    this.blog[key].goods_number = this.blog[key].goods_number - 1
-        // }
-        // alert(key+1)
-        // this.goodup.isgoodup = !this.goodup.isgoodup;
-        // console.log(this.blog[key].goods_number);
-        // this.blog[k] = res.data.goods_number
-        // this.axios.post('/showblog')
-        // .then(res=>{
-        //   console.log(res.data.data);
-        //   this.blog = res.data.data; 
-        // })
+  methods: {
+    switchbgi(key, item) {
+      this.agree.article_id = key;
+      // 1.点击时判断是否登录
+      if (localStorage.getItem("USER_ID")) {
+        this.agree.upuser_id = localStorage.getItem("USER_ID");
+      } else {
+        Dialog.confirm({
+          title: "",
+          message: "您还为登录哦,是否要登录",
+          showCancelButton: true
+        })
+          .then(() => {
+            this.$router.push("/login");
+          })
+          .catch(() => {});
       }
-  },
-  
-}
+
+      //2.点击时修改状态
+      if (this.agree.isgoodup == 0) {
+        this.agree.isgoodup = "1";
+        this.blog[item].goods_number = this.blog[item].goods_number + 1;
+      } else {
+        this.agree.isgoodup = "0";
+        this.blog[item].goods_number = this.blog[item].goods_number - 1;
+      }
+      //3.修改点赞对应的文章的点赞状态
+      this.axios.post("/supports", this.agree).then(res => {
+        this.agree.isgoodup = res.data.data.isgoodup;
+        // console.log(res.data.data);
+      });
+
+      // 4.修改表中点赞数量
+      this.axios
+        .post("/addagree/" + key, {
+          goods_number: this.blog[item].goods_number
+        })
+        .then(res => {
+          // console.log(res.data)
+        });
+    }
+  }
+};
 </script>
